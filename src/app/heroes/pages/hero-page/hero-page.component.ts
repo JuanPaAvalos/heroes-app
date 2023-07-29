79128bc54dcb6f9715eb00814fbf69ca1ffaefae
JuanPaAvalos <juanpablordz0524@gmail.com>
2023-07-29 15:55:02 -0500

    feat: implement get hero by id function in HeroPage

--- src/app/heroes/pages/hero-page/hero-page.component.ts
+++ src/app/heroes/pages/hero-page/hero-page.component.ts
@@ -1,16 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { HeroService } from '../../services/heroes.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { switchMap } from 'rxjs';
+import { Hero } from '../../interfaces/hero.interface';
 
 @Component({
   selector: 'app-hero-page',
   templateUrl: './hero-page.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class HeroPageComponent implements OnInit {
+  public hero?: Hero;
 
-  constructor() { }
+  constructor(
+    private heroService: HeroService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
+    this.activatedRoute.params
+      .pipe(switchMap(({ id }) => this.heroService.getHeroById(id)))
+      .subscribe((hero) => {
+        if(!hero) return this.router.navigateByUrl('heroes/list');
+        return this.hero = hero
+      });
   }
-
 }
