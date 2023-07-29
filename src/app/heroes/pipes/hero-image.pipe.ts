b0963dac31178a644e4f111fdb950c371e743ed2
JuanPaAvalos <juanpablordz0524@gmail.com>
2023-07-29 15:37:10 -0500

    feat: add hero image pipe

---
+++ src/app/heroes/pipes/hero-image.pipe.ts
@@ -0,0 +1,13 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { Hero } from '../interfaces/hero.interface';
+
+@Pipe({
+  name: 'heroImage',
+})
+export class HeroImagePipe implements PipeTransform {
+  transform(hero: Hero): string {
+    if (!hero.id && !hero.altImg) return 'assets/no-image.jpg';
+    if (hero.altImg) return hero.altImg;
+    return `assets/heroes/${hero.id}.jpg`;
+  }
+}
