44352658af758ac44f0b6fbb8a6f1c3af5351c00
JuanPaAvalos <juanpablordz0524@gmail.com>
2023-07-29 15:54:14 -0500

    feat: add function get hero by id in HeroService

--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Hero } from '../interfaces/hero.interface';
-import { Observable } from 'rxjs';
+import { Observable, catchError, of } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
 @Injectable({ providedIn: 'root' })
@@ -12,5 +12,13 @@ export class HeroService {
 
   getHeroes(): Observable<Hero[]> {
     return this.httpClient.get<Hero[]>(`${this.baseUrl}/heroes`);
+  }
+
+  getHeroById(id: string): Observable<Hero | undefined> {
+    return this.httpClient
+      .get<Hero | undefined>(`${this.baseUrl}/heroes/${id}`)
+      .pipe(
+        catchError( error => of (undefined)) //* Se utiliza "of (undefined)" para retornar un observable undefined y solo el valor undefined
+      );
   }
 }
