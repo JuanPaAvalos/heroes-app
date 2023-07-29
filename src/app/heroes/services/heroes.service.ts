8f1f592d56f4f5c11578b3267d8afa0d06f46a84
JuanPaAvalos <juanpablordz0524@gmail.com>
2023-07-29 02:29:29 -0500

    feat: add hero service
    add the get herores function

---
+++ src/app/heroes/services/heroes.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Hero } from '../interfaces/hero.interface';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+@Injectable({ providedIn: 'root' })
+export class HeroService {
+  private baseUrl: string = environment.baseUrl;
+
+  constructor(private httpClient: HttpClient) {}
+
+  getHeroes(): Observable<Hero[]> {
+    return this.httpClient.get<Hero[]>(`${this.baseUrl}/heroes`);
+  }
+}
