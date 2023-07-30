0d705fe92bb1e9a27fd9648e6c684f3930211a03
JuanPaAvalos <juanpablordz0524@gmail.com>
2023-07-30 11:31:53 -0500

    feat: add mat autocomplete

--- src/app/heroes/pages/search-page/search-page.component.ts
+++ src/app/heroes/pages/search-page/search-page.component.ts
@@ -1,16 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Hero } from '../../interfaces/hero.interface';
+import { HeroService } from '../../services/heroes.service';
+import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
 
 @Component({
   selector: 'app-search-page',
   templateUrl: './search-page.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class SearchPageComponent implements OnInit {
+  public searchInput = new FormControl('');
+  public heroes: Hero[] = [];
+  public selectedHero?: Hero;
 
-  constructor() { }
+  constructor(private heroService: HeroService) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  searchHero() {
+    const inputValue: string = this.searchInput.value || '';
+    this.heroService
+      .getSugestions(inputValue)
+      .subscribe((heroes) => (this.heroes = heroes));
   }
 
+  onSelectedOption(event: MatAutocompleteSelectedEvent) {
+    if (!event.option.value) this.selectedHero = undefined;
+
+    const hero: Hero = event.option.value;
+
+    this.searchInput.setValue(hero.superhero);
+    //TODO: cambiar esta asignacion por una peticion de get Hero By Id
+    this.selectedHero = hero;
+  }
 }
