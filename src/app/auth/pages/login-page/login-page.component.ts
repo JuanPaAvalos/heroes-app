d7db0329a867fbc87b1d85613eeed8e41eee9961
JuanPaAvalos <juanpablordz0524@gmail.com>
2023-08-04 01:05:06 -0500

    feat: implement login service in login component

--- src/app/auth/pages/login-page/login-page.component.ts
+++ src/app/auth/pages/login-page/login-page.component.ts
@@ -1,16 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class LoginPageComponent implements OnInit {
+  constructor(private authService: AuthService, private router: Router) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  onLogin() {
+    this.authService.login('juan@mail.com', '123456').subscribe((user) => {
+      this.router.navigateByUrl('heroes/list')
+    });
   }
-
 }
